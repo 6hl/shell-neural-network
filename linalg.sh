64ffe470ff60e3e6af3dec708da0a1dde3ddd690
Ben Liggett <bhliggett@gmail.com>
2022-07-09 17:07:15 -0500

    added inplace mult

--- linalg.sh
+++ linalg.sh
@@ -34,14 +34,14 @@ matrix_multiply(){
     #   col_2: num cols for matrix 2
     #
     # Returns:
-    #   Stores output into matrix_mul_out global variable, view output with: echo ${matrix_mul_out[@]}
+    #   Stores output into matrix_mult_out global variable, view output with: echo ${matrix_mult_out[@]}
     local -n _ar1=$1
     local -n _ar2=$2
     local row_1=$3
     local col_2=$4
     local result=0
     local idx=0
-    matrix_mul_out=()
+    matrix_mult_out=()
     for((x=0; x<$row_1; x++))
     do
         for((y=0; y<$col_2; y++))
@@ -51,8 +51,32 @@ matrix_multiply(){
             do
                 result=$(($result+${_ar1[$((x*$row_1+z))]} * ${_ar2[$((z*$col_2+y))]}))
             done
-            matrix_mul_out+=($result)
+            matrix_mult_out+=($result)
         done
+    done
+}
+
+inplace_multiplication() {
+    # Computes inplace multiplication of two matricies
+    #
+    # Args:
+    #   ar1 (n,m): matrix 1 formatted as an array
+    #   ar2 (n,m): matrix 2 formatted as an array
+    #   rows: num rows
+    #   cols: num cols
+    #
+    # Returns:
+    #   Stores output into inplace_mult_out global variable, view output with: echo ${inplace_mult_out[@]}
+    
+    local -n _ar1=$1
+    local -n _ar2=$2
+    local rows=$3
+    local cols=$4
+    local idx=0
+    inplace_mult_out=()
+    for i in "${!_ar1[@]}";
+    do
+        inplace_mult_out+=($((${_ar1[i]} * ${_ar2[i]})))
     done
 }
 
--- trainer.sh
+++ trainer.sh
@@ -6,14 +6,18 @@ train(){
     # Temp Train function for testing
     local ar1=(1 1 1 1 1 1 1 1 1)
     local ar2=(1 2 3 4 5 6 7 8 9)
+    local ar3=(1 2 3 4 5 6 7 8 9)
     matrix_addition ar1 ar2 3 3
     echo ${matrix_add_out[@]}
     matrix_multiply ar1 ar2 3 3
-    echo ${matrix_mul_out[@]}
+    echo ${matrix_mult_out[@]}
+    inplace_multiplication ar2 ar3 3 3
+    echo ${inplace_mult_out[@]}
 }
 
 declare -a matrix_add_out
-declare -a matrix_mul_out
+declare -a matrix_mult_out
+declare -a inplace_mult_out
 declare -a dot_prod_out
 
 train
