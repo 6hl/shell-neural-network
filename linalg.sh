724c2f9d5ad3fe7f17208af1bc01505c16d9a678
Ben Liggett <bhliggett@gmail.com>
2022-07-09 16:57:16 -0500

    fixed addition and added multiplication

--- linalg.sh
+++ linalg.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
-# matrix_multiply(){
-#     res_matrix=()
-#     for((x=0; x<row1; x++))
-#     do
-#         for((y=0; y<col2; y++))
-#         do
-#             for((z=0; z<row1; z++))
-#             do
-#                 idx1=$((x*$row1+z))
-#                 idx2=$((z*$col2+y))
-#                 result=$((result+${arr1[indx1]} * ${arr2[indx2]}))
-#             done
-#             matrix3+=($result)
-#         done
-#     done
-# }
-
-dot_product() {
-    echo Dot Product Incomplete
-}
 
 matrix_addition() {
     # Computes addition of two matricies
@@ -30,16 +10,52 @@ matrix_addition() {
     #   cols: num cols
     #
     # Returns:
-    #   Stores output into m global variable, view output with: echo ${matrix_add_out[@]}
-
+    #   Stores output into matrix_add_out global variable, view output with: echo ${matrix_add_out[@]}
+    
     local -n _ar1=$1
     local -n _ar2=$2
     local rows=$3
     local cols=$4
     local idx=0
-    for v in "${_ar1[@]}";
+    matrix_add_out=()
+    for i in "${!_ar1[@]}";
     do
-        matrix_add_out[idx]=$(($v+${_ar2[idx]}))
-        idx=$((idx+1))
+        matrix_add_out+=($((${_ar1[i]}+${_ar2[i]})))
     done
+}
+
+matrix_multiply(){
+    # Computes multiplication of two matricies
+    #
+    # Args:
+    #   ar1 (n,m): matrix 1 formatted as an array
+    #   ar2 (n,m): matrix 2 formatted as an array
+    #   row_1: num rows for matrix 1
+    #   col_2: num cols for matrix 2
+    #
+    # Returns:
+    #   Stores output into matrix_mul_out global variable, view output with: echo ${matrix_mul_out[@]}
+    local -n _ar1=$1
+    local -n _ar2=$2
+    local row_1=$3
+    local col_2=$4
+    local result=0
+    local idx=0
+    matrix_mul_out=()
+    for((x=0; x<$row_1; x++))
+    do
+        for((y=0; y<$col_2; y++))
+        do
+            result=0
+            for((z=0; z<$row_1; z++))
+            do
+                result=$(($result+${_ar1[$((x*$row_1+z))]} * ${_ar2[$((z*$col_2+y))]}))
+            done
+            matrix_mul_out+=($result)
+        done
+    done
+}
+
+dot_product() {
+    echo Dot Product Incomplete
 }
--- trainer.sh
+++ trainer.sh
@@ -8,9 +8,8 @@ train(){
     local ar2=(1 2 3 4 5 6 7 8 9)
     matrix_addition ar1 ar2 3 3
     echo ${matrix_add_out[@]}
-    matrix_addition matrix_add_out ar2 3 3
-    echo ${matrix_add_out[@]}
-    matrix_multiply
+    matrix_multiply ar1 ar2 3 3
+    echo ${matrix_mul_out[@]}
 }
 
 declare -a matrix_add_out
