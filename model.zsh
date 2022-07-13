b2be0428efa0c0362435db28bb1066265a6a5dd0
Ben Liggett <bhliggett@gmail.com>
2022-07-13 17:00:00 -0500

    fixed forward pass, setup iteration

--- model.zsh
+++ model.zsh
@@ -12,7 +12,7 @@ relu(){
     
     local _ar1=(${(P)1[@]})
     relu_out=()
-    for i in "${_ar1[@]}";
+    for i in $_ar1;
     do
         if [[ $i -lt 0 ]];
         then
@@ -36,11 +36,11 @@ softmax(){
     local e=2.7182818
     local sum=0
     softmax_out=()
-    for i in "${_ar1[@]}";
+    for i in $_ar1;
     do
         sum=$((sum+$e**$i))
     done
-    for i in "${_ar1[@]}";
+    for i in $_ar1;
     do
         softmax_out+=($(($e**$i/$sum)))
     done
@@ -50,30 +50,44 @@ forward_pass(){
     # Computes forward pass for inputs
     #
     # Args:
-    #   _ar1 (?,): vector
+    #   _ar1 (?,): one sample of data
     #
     # Returns:
     #   softmax output stored in softmax_out global variable, view output with: echo ${softmax_out[@]}
 
     local _ar1=(${(P)1[@]})                 # A0
-    matrix_multiply w1 _ar1                 # Output stored in matrix_mult_out
+    matrix_multiply w1 _ar1 10 1            # Output stored in matrix_mult_out
     matrix_addition matrix_mult_out b1      # Output stored in matrix_add_out
-    z1=matrix_add_out
+    z1=( $matrix_add_out )
     relu z1                                 # A1 stored in relu_out
-    matrix_multiply w2 relu_out             
+    matrix_multiply w2 relu_out 10 1      
     matrix_addition matrix_mult_out b2
-    z2=matrix_add_out
+    z2=( $matrix_add_out )
     softmax z2                              # A2 stored in softmax_out
 }
 
-gradient_descent(){
-    echo TODO: Complete gd
+stochastic_gradient_descent(){
+    echo TODO: Complete sgd
 }
 
 backprop(){
     echo TODO: Complete backpropogation
 }
 
-epoch(){
-    echo TODO: Complete single pass
+iteration(){
+    {   
+        read
+        while read l ; do
+            sample=()
+            val=255
+            clean=(${=$(echo "$l" | tr ',' ' ' | tr -s ' ')})
+            y_val=$clean[1]
+            shift clean
+            for i in $clean; do sample+=($(($i/255.0))); done
+            forward_pass sample 10 784
+            echo TODO: Add backprop
+            break
+        done 
+    } < fashion-mnist_train.csv
+
 }
--- trainer.zsh
+++ trainer.zsh
@@ -2,17 +2,6 @@
 source ./linalg.zsh
 source ./model.zsh
 
-load_data(){
-    {   
-        read
-        while read l ; do 
-            sample=(${=$(echo "$l" | tr ',' ' ' | tr -s ' ')})
-            break
-        done 
-    } < fashion-mnist_train.csv
-
-}
-
 declare -a matrix_add_out
 declare -a matrix_mult_out
 declare -a inplace_mult_out
@@ -21,11 +10,14 @@ declare -a dot_prod_out
 declare -a relu_out
 declare -a softmax_out
 
-declare -a w1=($(seq 0 .0001 .99 | shuf | head -n7850))
-declare -a b1=($(seq 0 .0001 .99 | shuf | head -n10))
-declare -a w2=($(seq 0 .0001 .99 | shuf | head -n100))
-declare -a b2=($(seq 0 .0001 .99 | shuf | head -n10))
+declare -a w1=($(seq 0 .0001 .99 | shuf | head -n7840)) # 10x784
+declare -a b1=($(seq 0 .0001 .99 | shuf | head -n10))   # 10x1
+declare -a w2=($(seq 0 .0001 .99 | shuf | head -n100))  # 10x10
+declare -a b2=($(seq 0 .0001 .99 | shuf | head -n10))   # 10x1
 declare -a z1
 declare -a z2
 
-load_data
+declare -a sample=()
+declare -a y_val
+
+iteration
